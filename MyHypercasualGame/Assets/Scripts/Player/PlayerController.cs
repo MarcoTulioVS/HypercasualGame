554f0f07bc4254025f18bca6b37311f6dfdc65d8
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2024-03-18 11:47:19 -0300

    Adding player controller

--- MyHypercasualGame/Assets/Scripts/Player/PlayerController.cs
+++ MyHypercasualGame/Assets/Scripts/Player/PlayerController.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [Header("Lerp")]
+
+    public Transform target;
+
+    public float lerpSpeed = 1f;
+
     public float speed;
+
+    private Vector3 _pos;
     private void Update()
     {
+        _pos = target.position;
+
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
     }
 }
